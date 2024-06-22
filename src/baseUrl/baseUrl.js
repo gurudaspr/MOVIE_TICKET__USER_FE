429e56a6ec00d530e985f7c057d9bfb9aa885912
gurudaspr <gurudaspranavam007@gmail.com>
2024-06-22 16:22:05 +0530

    edited user route

--- src/baseUrl/baseUrl.js
+++ src/baseUrl/baseUrl.js
@@ -1,4 +1,4 @@
 
-export const baseUrl = 'https://movieticketmainbe-production.up.railway.app';
+// export const baseUrl = 'https://movieticketmainbe-production.up.railway.app';
 // export const baseUrl = 'https://movie-ticket-main-be.onrender.com';
-// export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'http://localhost:5000';
