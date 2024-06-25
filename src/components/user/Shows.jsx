68f5ce8e486c47d280d1cb4a4ea68ce07858257e
gurudaspr <gurudaspranavam007@gmail.com>
2024-06-25 21:20:27 +0530

    time sort

--- src/components/user/Shows.jsx
+++ src/components/user/Shows.jsx
@@ -1,102 +1,91 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { baseUrl } from '../../baseUrl/baseUrl';
 import { movieTitleState } from '../../store/movieTitleAtom';
 import { useRecoilValue } from 'recoil';
 
-export default function Shows() {
-  const [shows, setShows] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const movieTitle = useRecoilValue(movieTitleState);
-  const { id } = useParams();
-  const navigate = useNavigate();
+export default function Shows({}) {
+        const [shows, setShows] = useState([]);
+        const [selectedDate, setSelectedDate] = useState(new Date());
+        const movieTitle = useRecoilValue(movieTitleState);
+        const { id } = useParams();
+        const navigate = useNavigate();
+        useEffect(() => {
+          const fetchShows = async () => {
+            try {
+              const formattedDate = selectedDate.toISOString().split('T')[0];
+              const response = await axios.get(`${baseUrl}/api/user/shows?date=${formattedDate}&movieId=${id}`, { withCredentials: true });
+              setShows(response.data);
+              console.log('Shows:', response.data);
+      
+            } catch (error) {
+              console.error('Error fetching shows:', error);
+            }
+          };
+      
+          fetchShows();
+        }, [selectedDate, id]);
+      
+        const handleDateChange = (date) => {
+          setSelectedDate(date);
+        };
+      
+        const renderDateTabs = () => {
+          const dateTabs = [];
+          for (let i = 0; i < 5; i++) {
+            const date = new Date();
+            date.setDate(date.getDate() + i);
+            dateTabs.push(
+              <button  
+                key={i} 
+                className={`px-4 py-2  rounded-md mr-2 border-none hover:bg-primary-hover text-primary-content ${selectedDate.toDateString() === date.toDateString() ? 'bg-primary' : ' bg-base-300'}`}
+                onClick={() => handleDateChange(date)}
+              >
+                {date.toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })}
+              </button>
+              
+            );
+          }
+          return dateTabs;
+        };
 
-  useEffect(() => {
-    const fetchShows = async () => {
-      try {
-        const formattedDate = selectedDate.toISOString().split('T')[0];
-        const response = await axios.get(`${baseUrl}/api/user/shows?date=${formattedDate}&movieId=${id}`, { withCredentials: true });
-        setShows(response.data);
-      } catch (error) {
-        console.error('Error fetching shows:', error);
-      }
-    };
-
-    fetchShows();
-  }, [selectedDate, id]);
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
-  const renderDateTabs = () => {
-    const dateTabs = [];
-    for (let i = 0; i < 5; i++) {
-      const date = new Date();
-      date.setDate(date.getDate() + i);
-      dateTabs.push(
-        <button
-          key={i}
-          className={`px-4 py-2 rounded-md mr-2 border-none hover:bg-primary-hover text-primary-content ${selectedDate.toDateString() === date.toDateString() ? 'bg-primary' : ' bg-base-300'}`}
-          onClick={() => handleDateChange(date)}
-        >
-          {date.toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })}
-        </button>
-      );
-    }
-    return dateTabs;
-  };
-
-  const ShowSeat = (showId) => {
-    navigate(`/showSeat/${showId}`);
-  };
-
-  const filterAndSortShowTimes = (showTimes) => {
-    showTimes.sort((a, b) => {
-      const [hourA, minuteA] = a.showTime.split(':').map(Number);
-      const [hourB, minuteB] = b.showTime.split(':').map(Number);
-      if (hourA !== hourB) {
-        return hourA - hourB;
-      } else {
-        return minuteA - minuteB;
-      }
-    });
-
-    return showTimes.map(({ showTime, showId }) => ({
-      showTime,
-      showId
-    }));
-  };
-
-  return (
-    <div className="container h-screen mx-auto px-5 md:px-10 py-20 animate-fade-in">
-      <h1 className="text-3xl font-semibold mb-4">{movieTitle}</h1>
-      <div className="flex mb-4">
-        {renderDateTabs()}
-      </div>
-      <div className="divider"></div>
-      <ul>
-        {shows.map((show) => (
-          <li key={`${show.theater}-${show.movieTitle}`} className="py-4">
-            <h2 className="text-xl font-semibold mb-1">{show.theater}</h2>
-            <span className='text-sm pt-5'>{show.theaterLocation}</span>
-            <p className="text-lg mb-2"></p>
-            <div className="flex flex-wrap">
-              {filterAndSortShowTimes(show.showTimes).map(({ showTime, showId }) => (
-                <button
-                  onClick={() => { ShowSeat(showId); }}
-                  key={showId}
-                  className="bg-base-200 hover:bg-base-300 text-gray-700 border-white rounded-lg w-20 h-10 mb-3 mr-1"
-                >
-                  {showTime}
-                </button>
-              ))}
+        const ShowSeat = (showId) => {
+          navigate(`/showSeat/${showId}`);
+        }
+        
+      
+        return (
+          <div className="container h-screen mx-auto  px-5 md:px-10 py-20 animate-fade-in">
+            <h1 className="text-3xl font-semibold mb-4">{movieTitle}</h1>
+            <div className="flex mb-4">
+              {renderDateTabs()}
             </div>
             <div className="divider"></div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+            <ul>
+              
+            {shows.map((show) => (
+              <li key={`${show.theater}-${show.movieTitle}`} className=" py-4">
+                <h2 className="text-xl font-semibold mb-1">{show.theater}</h2>
+                <span className='text-sm pt-5'>{show.theaterLocation}</span>
+            
+                <p className="text-lg mb-2"></p>
+                <div className="flex flex-wrap">
+                {show.showTimes.map(({ showTime, showId }, index) => (
+                      <button  onClick={() => {ShowSeat(showId)}}
+                        key={showId} 
+                        className="bg-base-200 hover:bg-base-300 text-gray-700 border-white rounded-lg w-20 h-10 mb-3 mr-1"
+                      >
+                        {showTime}
+                        
+                      </button>
+                  ))}
+                </div>
+                <div className="divider"></div>
+              </li>
+            ))}
+            </ul>
+          </div>
+        );
+      };
+
